b645aec15965857916b847ac1bb22eaf2af979d3
thiagocorrea-hotmart <86610816+thiagocorrea-hotmart@users.noreply.github.com>
2023-06-28 22:58:40 -0300

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import './style.scss'
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
-import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from 'react';
-import api from '../../services/'
-import { Input, TextField } from '@mui/material';
+import { ChangeEvent, MouseEvent, useRef, useState } from 'react';
+import { TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ReactGA from "react-ga4";
 
 type IHomeProps = {
     event?: MouseEvent<HTMLImageElement, MouseEvent>
@@ -20,8 +21,12 @@ export const Home = () => {
 
     const alexa = 'https://m.media-amazon.com/images/I/61gufBsGK7L._AC_SL1000_.jpg'
     const [eventImagem, setEventImage] = useState<Array<IHomeProps>>([])
+    const history = useNavigate();
 
 
+    const redirectToContactPage = () => {
+        history('/contact')
+    }      
 
     const handlerClickImage = (event: MouseEvent<HTMLImageElement, globalThis.MouseEvent>) => {
         
@@ -41,9 +46,7 @@ export const Home = () => {
 
     const refDoelement = useRef<any>(null)
 
-    const handlerClickButton = (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
-        console.log(e)
-    }
+    
 
     const handleInput = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         e.preventDefault()
@@ -51,10 +54,18 @@ export const Home = () => {
         console.log(e)
     }
 
-    // useEffect(() => {
-    //     api.post('event-img', eventImagem)
-    // })
+   
+    const handlerClickButton = (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+        e.preventDefault() 
 
+        ReactGA.event({
+            category: 'Cart',
+            action: 'Add to Cart',
+            label: 'Button Clicked'
+        });
+    }
+
+    
     return (
         <div>
             <TextField 
@@ -63,6 +74,10 @@ export const Home = () => {
                 variant="outlined"
                 onChange={(event) => handleInput(event)}
             />
+            <Button variant="contained" onClick={redirectToContactPage} className="btnRedirect">
+                Contate-nos 
+            </Button>
+
              <div className='container'>
                 <div>
                     <img 
@@ -74,82 +89,81 @@ export const Home = () => {
                     />
                 </div>
                 <div>
-                <Card className='price' sx={{ minWidth: 275 }}>
+                    <Card className='price' sx={{ minWidth: 275 }}>
 
-                    <div className='description'>
-                        <h3>Echo Dot (3ª Geração): Smart Speaker com Alexa - Cor Preta</h3>
-                        <span>Marca: Amazon</span>
-                        <span>128.901 avaliações de clientes</span>
-                        <span>1000+perguntas respondidas</span>
-                    </div>
-                    <div className='description-price'>
-                        <h2>215,55</h2>
-                        <span>à vista (5% off) ou R$ 229,00 em até 11x de R$ 20,90 sem juros </span>
-                    </div>
-                    <div className='description-text'>
-                        <ul>
-                            <li>
-                                O Echo Dot é o nosso smart speaker de maior sucesso. 
-                                Controlado por voz com a Alexa, 
-                                ele é perfeito para qualquer ambiente. 
-                                Você pode pedir músicas, notícias, informações e muito mais. 
-                                Além de ligar para amigos e familiares e controlar dispositivos compatíveis 
-                                de casa inteligente com sua voz.
-                            </li>
-                            <li>
-                                Peça para a Alexa tocar músicas, responder perguntas, ler as notícias, 
-                                checar a previsão do tempo, criar alarmes, controlar 
-                                dispositivos de casa inteligente compatíveis 
-                                e muito mais.
-                            </li>
-                            <li>
-                                Ouça músicas do Amazon Music, Apple Music, Spotify, 
-                                Deezer e outros usando o alto-falante integrado com um 
-                                som envolvente.
-                            </li>
-                            <li>
-                                Faça chamadas para o aplicativo Alexa ou outros dispositivos
-                                Echo compatíveis sem usar suas mãos. Envie avisos para outros 
-                                dispositivos Echo em sua casa.
-                            </li>
-                            <li>
-                                A Alexa está sempre aprendendo e adicionando novas Skills, como jogos, notícias e muito mais.
-                            </li>
-                            <li>
-                                Com 4 microfones de longo alcance, a Alexa ouve você do outro lado do cômodo. 
-                                E com dispositivos Echo compatíveis em diferentes cômodos, você pode ouvir música em toda a sua casa. 
-                            </li>
-                            <li>
-                                Também é possível conectar o Echo aos seus próprios alto-falantes por Bluetooth ou com um cabo de áudio de 3,5 mm.
-                            </li>
-                            <li>
-                                Use sua voz para acender as luzes, trancar as portas e muito mais com dispositivos de casa inteligente compatíveis.
-                            </li>
+                        <div className='description'>
+                            <h3>Echo Dot (3ª Geração): Smart Speaker com Alexa - Cor Preta</h3>
+                            <span>Marca: Amazon</span>
+                            <span>128.901 avaliações de clientes</span>
+                            <span>1000+perguntas respondidas</span>
+                        </div>
+                        <div className='description-price'>
+                            <h2>215,55</h2>
+                            <span>à vista (5% off) ou R$ 229,00 em até 11x de R$ 20,90 sem juros </span>
+                        </div>
+                        <div className='description-text'>
+                            <ul>
+                                <li>
+                                    O Echo Dot é o nosso smart speaker de maior sucesso. 
+                                    Controlado por voz com a Alexa, 
+                                    ele é perfeito para qualquer ambiente. 
+                                    Você pode pedir músicas, notícias, informações e muito mais. 
+                                    Além de ligar para amigos e familiares e controlar dispositivos compatíveis 
+                                    de casa inteligente com sua voz.
+                                </li>
+                                <li>
+                                    Peça para a Alexa tocar músicas, responder perguntas, ler as notícias, 
+                                    checar a previsão do tempo, criar alarmes, controlar 
+                                    dispositivos de casa inteligente compatíveis 
+                                    e muito mais.
+                                </li>
+                                <li>
+                                    Ouça músicas do Amazon Music, Apple Music, Spotify, 
+                                    Deezer e outros usando o alto-falante integrado com um 
+                                    som envolvente.
+                                </li>
+                                <li>
+                                    Faça chamadas para o aplicativo Alexa ou outros dispositivos
+                                    Echo compatíveis sem usar suas mãos. Envie avisos para outros 
+                                    dispositivos Echo em sua casa.
+                                </li>
+                                <li>
+                                    A Alexa está sempre aprendendo e adicionando novas Skills, como jogos, notícias e muito mais.
+                                </li>
+                                <li>
+                                    Com 4 microfones de longo alcance, a Alexa ouve você do outro lado do cômodo. 
+                                    E com dispositivos Echo compatíveis em diferentes cômodos, você pode ouvir música em toda a sua casa. 
+                                </li>
+                                <li>
+                                    Também é possível conectar o Echo aos seus próprios alto-falantes por Bluetooth ou com um cabo de áudio de 3,5 mm.
+                                </li>
+                                <li>
+                                    Use sua voz para acender as luzes, trancar as portas e muito mais com dispositivos de casa inteligente compatíveis.
+                                </li>
 
-                        </ul>
-                    </div>
-                </Card>
-            </div>
-            <div>
-                <Card className='price' sx={{ minWidth: 275 }}>
-                    <h1>R$217,55</h1>
-                    <span>à vista</span> 
-                    <p>Ou em até 11x de R$ 20,90/mês</p> 
-                    <p>Entrega GRÁTIS: Quarta-feira, 29 de Junho</p>
-                    <p>Ou Entrega mais rápida: Terça-feira, 21 de Junho</p>
-                    <h1>Em estoque</h1>
-                    <p>Quantidade: 10</p>
-                    <Button 
-                        className="button-add" 
-                        variant="contained" 
-                        onClick={(event) => handlerClickButton(event)}                    
-                    >
-                        Adicionar no carrinho
-                    </Button>
-                </Card>
+                            </ul>
+                        </div>
+                    </Card>
+                </div>
+                <div>
+                    <Card className='price' sx={{ minWidth: 275 }}>
+                        <h1>R$217,55</h1>
+                        <span>à vista</span> 
+                        <p>Ou em até 11x de R$ 20,90/mês</p> 
+                        <p>Entrega GRÁTIS: Quarta-feira, 29 de Junho</p>
+                        <p>Ou Entrega mais rápida: Terça-feira, 21 de Junho</p>
+                        <h1>Em estoque</h1>
+                        <p>Quantidade: 10</p>
+                        <Button 
+                            className="button-add" 
+                            variant="contained" 
+                            onClick={(event) => handlerClickButton(event)}                    
+                        >
+                            Adicionar no carrinho
+                        </Button>
+                    </Card>
+                </div>
             </div>
         </div>
-        </div>
-    )
-}
+    )}
 
